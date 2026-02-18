e0577c29d1ba1824f2fc833d96edc5862a7c2757
Thando <njabulodhladla8@gmail.com>
2026-02-18 14:05:39 +0200

    Unresolved buttons

--- comchat/lib/main.dart
+++ comchat/lib/main.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:logging/logging.dart';
 import 'firebase_options.dart';
 import 'theme.dart';
 import 'auth_gate.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  _setupLogging();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const MyApp());
+}
+
+void _setupLogging() {
+  Logger.root.level = Level.ALL;
+  Logger.root.onRecord.listen((rec) {
+    print('${rec.level.name}: ${rec.time}: ${rec.message}');
+  });
 }
 
 class MyApp extends StatelessWidget {
